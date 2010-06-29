2ab3793ce79c6a8ace0d015aa4fe11bb53b58097
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2010-06-29 14:42:16 -0700

    adding some gems to enable user auth

---
+++ tools/jruby-1.5.1/lib/ruby/gems/1.8/gems/simplest_auth-0.2.6/lib/simplest_auth/version.rb
@@ -0,0 +1,13 @@
+module SimplestAuth
+  module Version
+    
+    MAJOR = 0
+    MINOR = 2
+    TINY  = 6
+    
+    def self.to_s # :nodoc:
+      [MAJOR, MINOR, TINY].join('.')
+    end
+    
+  end
+end
---
+++ tools/jruby-1.5.1/lib/ruby/gems/1.8/gems/simplest_auth-0.2.6/test/unit/simplest_auth/model_test.rb
@@ -0,0 +1,85 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class User; end
+
+class UserTest < Test::Unit::TestCase
+  include BCrypt
+
+  context "with no ORM" do
+    setup do
+      User.stubs(:active_record?).returns(false)
+      User.stubs(:data_mapper?).returns(false)
+      User.send(:include, SimplestAuth::Model)
+    end
+
+    should "return nil for authenticate" do
+      assert_equal nil, User.authenticate('email', 'password')
+    end
+  end
+
+  context "The User class" do
+    should "define a session key" do
+      assert_equal :user_id, User.session_key
+    end
+  end
+
+  context "An instance of the User class" do
+    setup do
+      User.send(:include, SimplestAuth::Model)
+      @user = User.new
+      @user.stubs(:crypted_password).returns('abcdefg')
+    end
+
+    should "determine if a password is authentic" do
+      password_stub = stub
+      password_stub.stubs(:==).with('password').returns(true)
+      Password.stubs(:new).with('abcdefg').returns(password_stub)
+
+      assert @user.authentic?('password')
+    end
+
+    should "determine when a password is not authentic" do
+      password_stub = stub
+      password_stub.stubs(:==).with('password').returns(false)
+      Password.stubs(:new).with('abcdefg').returns(password_stub)
+      
+      assert_equal false, @user.authentic?('password')
+    end
+
+    should "use the Password class == method for comparison" do
+      password_stub = mock
+      password_stub.expects(:==).with('password').returns(true)
+      Password.stubs(:new).with('abcdefg').returns(password_stub)
+      
+      @user.authentic?('password')
+    end
+
+    should "use a new Password made from crypted_password" do
+      password_stub = stub
+      password_stub.stubs(:==).with('password').returns(true)
+      Password.expects(:new).with('abcdefg').returns(password_stub)
+      
+      @user.authentic?('password')
+    end
+
+    should "hash a password using bcrypt" do
+      @user.stubs(:password_required?).returns(true)
+      @user.expects(:crypted_password=).with('abcdefg')
+      @user.password = 'password'
+      Password.expects(:create).with('password').returns('abcdefg')
+
+      @user.send(:hash_password)
+    end
+
+    should "require a password if crypted password is blank" do
+      @user.stubs(:crypted_password).returns(stub(:blank? => true))
+      assert_equal true, @user.send(:password_required?)
+    end
+
+    should "require a password if a password has been set" do
+      @user.stubs(:crypted_password).returns(stub(:blank? => false))
+      @user.stubs(:password).returns(stub(:blank? => false))
+      assert_equal true, @user.send(:password_required?)
+    end
+  end
+end
