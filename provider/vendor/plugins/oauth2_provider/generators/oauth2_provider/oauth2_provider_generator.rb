4ff4ce2a91450470a692d97ebd8bc3851464f622
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2010-07-20 13:16:58 -0700

    Add a generator plugin that generates the right db migrations and initializer when executed.

---
+++ provider/vendor/plugins/oauth2_provider/generators/oauth2_provider/oauth2_provider_generator.rb
@@ -0,0 +1,25 @@
+class Oauth2ProviderGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
+      m.template 'config/initializers/oauth2_provider.rb', "config/initializers/oauth2_provider.rb"
+      
+      ['create_oauth_clients', 'create_oauth_tokens', 'create_oauth_authorizations'].each_with_index do |file_name, index|
+        m.template "db/migrate/#{file_name}.rb", "db/migrate/#{version_with_prefix(index)}_#{file_name}.rb", :migration_file_name => file_name
+      end
+      
+    end
+  end
+  
+  def after_generate
+    puts "*"*80
+    puts "Please edit the file 'config/initializers/oauth2_provider.rb' as per your needs!"
+    puts "The readme file in the plugin contains more information about configuration."
+    puts "*"*80
+  end
+  
+  private
+  def version_with_prefix(prefix)
+    Time.now.utc.strftime("%Y%m%d%H%M%S") + "#{prefix}"
+  end
+  
+end
---
+++ provider/vendor/plugins/oauth2_provider/generators/oauth2_provider/templates/config/initializers/oauth2_provider.rb
@@ -0,0 +1,14 @@
+module Oauth2
+  module Provider
+
+    raise 'OAuth2 provider not configured yet!'
+    # please go through the readme and configure this file before you can use this plugin!
+    
+    # make sure no authentication for OauthTokenController
+    OauthTokenController.skip_before_filter(:login_required)
+    
+    # use host app's custom authorization filter to protect OauthClientsController
+    OauthClientsController.before_filter(:ensure_admin_user)
+    
+  end
+end
