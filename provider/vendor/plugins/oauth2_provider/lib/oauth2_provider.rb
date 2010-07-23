961a5ddaffec4e96fd47bfe0b9a38de31c464563
David Rice <david.rice@gmail.com>
2010-07-23 11:31:00 -0700

    fix development environment hack

--- provider/vendor/plugins/oauth2_provider/lib/oauth2_provider.rb
+++ provider/vendor/plugins/oauth2_provider/lib/oauth2_provider.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2010 ThoughtWorks Inc. (http://thoughtworks.com)
 # Licenced under the MIT License (http://www.opensource.org/licenses/mit-license.php)
 
-#
-# DO NOT DELETE
-# rubygems seems to require this file!
-#
+# hack so this works in dev
+if RAILS_ENV == 'development'
+  ActiveSupport::Dependencies.load_once_paths.reject!{|x| x =~ /^#{Regexp.escape(File.dirname(File.dirname(__FILE__)))}/}
+end
