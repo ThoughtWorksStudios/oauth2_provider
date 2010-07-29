f9abe50bd26a23ce2ee77135e84075d97f15aa00
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2010-07-29 10:24:23 -0700

    KP/WPC - Vendor validatable gem.

--- provider/config/environment.rb
+++ provider/config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
+  config.gem 'validatable', :version => '1.6.7'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
