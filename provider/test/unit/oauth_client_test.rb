040f33ea5b5d5f18681924ee8f52df61cea3bed7
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2010-07-28 17:10:19 -0700

    KP/WPC - fix a test that was never running because it was not prefixed with test_

--- provider/test/unit/oauth_client_test.rb
+++ provider/test/unit/oauth_client_test.rb
@@ -5,20 +5,20 @@ require File.join(File.expand_path(File.dirname(__FILE__)), '/../test_helper')
 module Oauth2
   module Provider
     class OauthClientTest < ActiveSupport::TestCase
-      def should_generate_client_id_and_client_secret_when_creating_clients
+      def test_should_generate_client_id_and_client_secret_when_creating_clients
         client = OauthClient.create!(:name => 'foobar', :redirect_uri => 'http://example.com/cb')
         assert_not_nil client.client_id
         assert_not_nil client.client_secret
       end
   
-      def should_not_allow_creating_clients_without_a_name
+      def test_should_not_allow_creating_clients_without_a_name
         assert_raise_with_message ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank" do
           OauthClient.create!(:redirect_uri => 'http://example.com/cb')
         end
       end
   
-      def should_not_allow_creating_clients_without_a_redirct_uri
-        assert_raise_with_message ActiveRecord::RecordInvalid, "Validation failed: Redirect uri can't be blank" do
+      def test_should_not_allow_creating_clients_without_a_redirct_uri
+        assert_raise_with_message ActiveRecord::RecordInvalid, "Validation failed: Redirect uri can't be blank, Redirect uri is invalid" do
           OauthClient.create!(:name => 'foobar')
         end
       end
