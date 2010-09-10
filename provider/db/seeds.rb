69e334b3797b8ba17fc48bddd53983b4490807c4
Bill DePhillips <bill.dephillips@gmail.com>
2010-09-10 12:05:34 -0700

    add seed data for hosting sample app

--- provider/db/seeds.rb
+++ provider/db/seeds.rb
@@ -5,3 +5,4 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+User.create!(:email => 'admin', :password => 'p')
