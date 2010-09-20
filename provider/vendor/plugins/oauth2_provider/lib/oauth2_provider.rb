474fff96220ab6e952869221c223e435f6e08195
Bill DePhillips <bill.dephillips@gmail.com>
2010-09-20 16:06:55 -0700

    removed datasource message

--- provider/vendor/plugins/oauth2_provider/lib/oauth2_provider.rb
+++ provider/vendor/plugins/oauth2_provider/lib/oauth2_provider.rb
@@ -8,12 +8,6 @@ require 'oauth2/provider/clock'
 
 Oauth2::Provider::ModelBase.datasource = ENV["OAUTH2_PROVIDER_DATASOURCE"]
 
-unless ENV['LOAD_OAUTH_SILENTLY']
-  puts "*"*80
-  puts "*** Using data source: #{Oauth2::Provider::ModelBase.datasource.class}"
-  puts "*"*80
-end
-
 Dir[File.join(File.dirname(__FILE__), "..", "app", "**", '*.rb')].each do |rb_file|
   require File.expand_path(rb_file)
 end
