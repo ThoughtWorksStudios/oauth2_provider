a5e0754119942b76855f0655e6a68d08ab5a3e0e
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2010-09-27 13:51:50 -0700

    Add a sslhelper to force SSL access on controllers that include the ssl_helper.

---
+++ provider/test/unit/ssl_helper_test.rb
@@ -0,0 +1,88 @@
+# Copyright (c) 2010 ThoughtWorks Inc. (http://thoughtworks.com)
+# Licenced under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+
+require File.join(File.expand_path(File.dirname(__FILE__)), '/../test_helper')
+
+module Oauth2
+  module Provider
+    class SslHelperController < ActionController::Base
+      include SslHelper
+
+      before_filter :mandatory_ssl, :only => [:a, :b]
+
+      def a
+        render :nothing => true
+      end
+
+      def b
+        render :nothing => true
+      end
+
+      def c
+        render :nothing => true
+      end
+
+      def d
+        render :nothing => true
+      end
+
+      def rescue_action(e)
+        raise e
+      end
+    end
+
+    class SslRequirementTest < ActionController::TestCase
+      def setup
+        @controller = SslHelperController.new
+        @request    = ActionController::TestRequest.new
+        @response   = ActionController::TestResponse.new
+      end
+
+      def teardown
+        ENV['OAUTH_SSL_PORT'] = nil
+      end
+
+      def test_accessing_ssl_forced_actions_redirect_to_ssl_when_request_is_not_ssl
+        @controller.instance_variable_set('@ssl_enabled', true)
+        ENV['OAUTH_SSL_PORT'] = '8443'
+        assert_not_equal "on", @request.env["HTTPS"]
+        get :a
+        assert_response :redirect
+        assert_match %r{^https://}, @response.headers['Location']
+        get :b
+        assert_response :redirect
+        assert_match %r{^https://}, @response.headers['Location']
+      end
+
+      def test_accessing_ssl_forced_actions_do_not_redirect_when_request_is_ssl
+        @request.env['HTTPS'] = "on"
+        get :a
+        assert_response :success
+        get :b
+        assert_response :success
+      end
+
+      def test_non_ssl_actions_are_available_without_ssl
+        assert_not_equal "on", @request.env["HTTPS"]
+        get :d
+        assert_response :ok
+      end
+
+      def test_non_ssl_actions_are_available_with_ssl
+        @request.env['HTTPS'] = "on"
+        get :d
+        assert_response :ok
+      end
+
+      def test_ssl_forced_actions_redirect_to_error_page_when_ssl_is_disabled
+        assert_not_equal "on", @request.env["HTTPS"]
+        @controller.instance_variable_set('@ssl_enabled', false)
+        assert !@controller.send(:ssl_enabled?)
+        get :a
+        assert_response :forbidden
+      end
+
+    end
+  end
+end
+
---
+++ provider/vendor/plugins/oauth2_provider/lib/oauth2/provider/ssl_helper.rb
@@ -0,0 +1,45 @@
+# Copyright (c) 2010 ThoughtWorks Inc. (http://thoughtworks.com)
+# Licenced under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+
+module Oauth2
+  module Provider
+    module SslHelper
+
+      def self.included(controller_class)
+        controller_class.before_filter(:mandatory_ssl) unless ENV['DISABLE_SSL']
+      end
+
+      private
+      def mandatory_ssl
+        if !request.ssl?
+          if !ssl_enabled?
+            error = 'This page can only be accessed using HTTPS.'
+            error += render_help_link('Configure SSL', :class => '') if defined?(render_help_link)
+            flash.now[:error] = error
+            render(:text => '', :layout => true, :status => :forbidden)
+            return false
+          else
+            redirect_to 'https://' + request.host + ':' + ssl_port + request.request_uri
+            return false
+          end
+        end
+        true
+      end
+
+      def ssl_enabled?
+        # this is set from within tests!
+        unless @ssl_enabled.nil?
+          @ssl_enabled
+        else
+          !ssl_port.nil?
+        end
+      end
+
+      def ssl_port
+        ENV['OAUTH_SSL_PORT']
+      end
+
+    end
+  end
+end
+
