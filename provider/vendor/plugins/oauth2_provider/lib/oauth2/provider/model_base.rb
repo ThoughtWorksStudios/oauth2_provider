083a2a79b005c3bde762758dc00be253e8dedce5
Luau <luaupair@luau-pair-02.corporate.thoughtworks.com>
2010-12-01 13:51:21 -0800

    require validatable explicitly because const_missing not work correctly in thread safe mode

--- provider/vendor/plugins/oauth2_provider/lib/oauth2/provider/model_base.rb
+++ provider/vendor/plugins/oauth2_provider/lib/oauth2/provider/model_base.rb
@@ -1,5 +1,6 @@
 # Copyright (c) 2010 ThoughtWorks Inc. (http://thoughtworks.com)
 # Licenced under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+require 'validatable'
 
 module Oauth2
   module Provider
