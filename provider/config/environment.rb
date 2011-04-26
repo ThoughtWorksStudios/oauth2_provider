d54702f194edd05389968cf8947465860abccc5d
Luau <luaupair@sfstdluaupair02.thoughtworks.com>
2011-04-26 16:39:11 -0700

    Print out the environment when starting up the server.

--- provider/config/environment.rb
+++ provider/config/environment.rb
@@ -43,3 +43,7 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+puts "*"*72
+puts "Rails environment is #{RAILS_ENV}"
+puts "*"*72
